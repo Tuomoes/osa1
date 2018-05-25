6966e0c89ab1f8a0311d8c5de881b3de88ad43d0
NJINFRA\eskoltuo <tuomo.eskola@nestejacobs.com>
2018-05-25 15:27:02 +0300

    subtask 1.6 done

--- lisaa_reactia/src/index.js
+++ lisaa_reactia/src/index.js
@@ -1,35 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const title1 = 'anna palautetta'
-  const titl2 = 'statistiikka'
-  
-  const hyva = 'hyvä'
-  const hyvaCount = 0
-  const neutraali = 'neutraali'
-  const neutraaliCount = 0
-  const huono = 'huono'
-  const huonoCount = 0
 
-  return (
-    <div>
-      <h1>{title1}</h1>
-      <button title='HYVÄ BUTTON'>
-        hyvä
-      </button>
-      <button title='NEUTRAALI BUTTON'>
-        neutraali
-      </button>
-      <button title='HUONO BUTTON'>
-        huono
-      </button>
-      <h1>{titl2}</h1>
-      <p>{hyva} {hyvaCount}</p>
-      <p>{neutraali} {neutraaliCount}</p>
-      <p>{huono} {huonoCount}</p>
-    </div>
-  )
+
+class App extends React.Component {
+  
+    constructor() {
+        super()
+        this.title1 = 'anna palautetta'
+        this.titl2 = 'statistiikka'
+        
+        this.hyva = 'hyvä'
+        this.neutraali = 'neutraali'
+        this.huono = 'huono'
+        this.state = {
+            hyvaCounter: 0,
+            neutraaliCount: 0,
+            huonoCount: 0
+        }
+      }
+  
+
+  
+  increaseHyva = () => {
+    this.setState({ hyvaCounter: this.state.hyvaCounter + 1 })
+  }
+
+  increaseNeutraali = () => {
+    this.setState({ neutraaliCount: this.state.neutraaliCount + 1 })
+  }
+
+  increaseHuono = () => {
+      this.setState({ huonoCount: this.state.huonoCount + 1})
+  }
+
+
+  render() {
+    return (
+        <div>
+          <h1>{this.title1}</h1>
+          <button title='HYVÄ BUTTON' onClick={this.increaseHyva.bind(this)}> 
+            hyvä
+          </button>
+          <button title='NEUTRAALI BUTTON' onClick={this.increaseNeutraali.bind(this)}>
+            neutraali
+          </button>
+          <button title='HUONO BUTTON' onClick={this.increaseHuono.bind(this)}>
+            huono
+          </button>
+          <h1>{this.titl2}</h1>
+          <p>{this.hyva} {this.state.hyvaCounter}</p>
+          <p>{this.neutraali} {this.state.neutraaliCount}</p>
+          <p>{this.huono} {this.state.huonoCount}</p>
+        </div>
+      )
+  }
 }
 
   
